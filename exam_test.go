3bd86a2ba6e793a53b1cb1d287be5c4bb9a9a590
发条 <328042356@qq.com>
2017-05-15 22:12:05 +0800

    Update exam_test.go

--- exam_test.go
+++ exam_test.go
@@ -1,5 +1,17 @@
+package main
 import (
     "testing"
 )
 
-func Test_base64()
+func Test_base64(){
+    str := "test"
+    if string(base64Decode(base64Encode([]byte(str))))!=str{
+        t.Error("test failed")
+    }
+}
+
+
+func Test_test(){
+    test()
+    t.Log("test successed")
+}
